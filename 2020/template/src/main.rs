3ff87a5d42b8768c21962a21f7ad677357d52323
Robert Katzki <katzki@ubilabs.net>
2020-12-02 17:37:24 +0100

    feat: add template

---
+++ 2020/template/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let entries = helper::read_file_strings().unwrap();
+
+    part1(&entries);
+    part2(&entries);
+}
+
+fn part1(entries: &Vec<String>) {
+    println!("Part 1: {}.", entries);
+}
+
+fn part2(entries: &Vec<String>) {
+    println!("Part 2: {}.", entries);
+}
