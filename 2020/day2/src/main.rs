4f4d3fdab9fb664b0f4b9672637aab534e05f6b8
Robert Katzki <katzki@ubilabs.net>
2020-12-02 12:09:14 +0100

    day2 2020

---
+++ 2020/day2/src/main.rs
@@ -0,0 +1,56 @@
+use std::fs;
+use regex::Regex;
+
+struct Entry {
+  min: usize,
+  max: usize,
+  letter: char,
+  password: String
+}
+
+fn main() {
+  let raw_entries = read_file().unwrap();
+
+  let re = Regex::new(r"^(\d+)-(\d+) ([a-z]{1}): ([a-z]+)$").unwrap();
+
+  let entries: Vec<Entry> = raw_entries
+    .iter()
+    .map(|entry| {
+      let cap = re.captures(entry).unwrap();
+      return Entry {
+          min: cap[1].parse::<usize>().unwrap(),
+          max: cap[2].parse::<usize>().unwrap(),
+          letter: cap[3].to_string().chars().nth(0).unwrap(),
+          password: cap[4].to_string()
+        }})
+    .collect();
+
+  part1(&entries);
+  part2(&entries);
+}
+
+fn part1(entries: &Vec<Entry>) {
+  let valid_passwords = entries.iter()
+    .filter(|entry| {
+      let letter_count = entry.password.chars().filter(|c| c == &entry.letter).count();
+      return letter_count >= entry.min && letter_count <= entry.max;
+    })
+    .count();
+
+  println!("Part 1: {} valid passwords.", valid_passwords);
+}
+
+fn part2(entries: &Vec<Entry>) {
+  let valid_passwords = entries.iter()
+    .filter(|entry|
+      entry.password.chars().nth(entry.min) == Some(entry.letter)
+      || entry.password.chars().nth(entry.max) == Some(entry.letter))
+    .count();
+
+  println!("Part 2: {} valid passwords.", valid_passwords);
+}
+
+fn read_file() -> std::io::Result<Vec<String>> {
+  let data = fs::read_to_string("input.txt")?.lines().map(|v| v.to_string()).collect();
+  return Ok(data);
+}
