108bd574a0f0ad5987e5979af08903c7a4408338
Robert Katzki <katzki@ubilabs.net>
2020-12-03 13:13:09 +0100

    refactor: optimize day3 2020

--- 2020/day3/src/main.rs
+++ 2020/day3/src/main.rs
@@ -1,47 +1,25 @@
 fn main() {
     let entries = helper::read_file_strings().unwrap();
 
-    part1(&entries);
-    part2(&entries);
-}
+    println!(
+        "Part 1: {} trees hit.",
+        get_hit_trees_for_slope(&entries, &(3, 1))
+    );
 
-fn part1(entries: &Vec<String>) {
-    let hit_trees = get_hit_trees_for_slope(entries, &(3, 1));
-    println!("Part 1: {} trees hit.", hit_trees);
-}
-
-fn part2(entries: &Vec<String>) {
-    let slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
-    let hit_trees_per_slope: usize = slopes
-        .iter()
-        .map(|s| get_hit_trees_for_slope(entries, s))
-        .fold(1, |acc, x| acc * x);
-
-    println!("Part 2: {}.", hit_trees_per_slope);
+    println!(
+        "Part 2: {}.",
+        [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+            .iter()
+            .map(|s| get_hit_trees_for_slope(&entries, s))
+            .fold(1, |acc, x| acc * x)
+    );
 }
 
 fn get_hit_trees_for_slope(entries: &Vec<String>, diff: &(usize, usize)) -> usize {
-    let mut x: usize = 0;
-    let mut y: usize = 0;
-    let mut hit_trees: usize = 0;
-
-    while y < entries.len() {
-        let normalized_x = if x >= entries[y].len() {
-            x % entries[y].len()
-        } else {
-            x
-        };
-        let symbol = entries[y].chars().nth(normalized_x).unwrap();
-
-        if symbol == '#' {
-            hit_trees += 1;
-        }
-
-        x += diff.0;
-        y += diff.1;
-    }
-
-    println!("Slope {} {} hit {} trees.", diff.0, diff.1, hit_trees);
-
-    return hit_trees;
+    entries
+        .iter()
+        .step_by(diff.1)
+        .zip((0..entries.len() * diff.0).step_by(diff.0))
+        .filter(|&(row, x)| row.chars().cycle().nth(x).unwrap() == '#')
+        .count()
 }
