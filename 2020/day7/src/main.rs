afd966a2a7747c04f83f7b44ba8aa1f70c26241a
Robert Katzki <katzki@ubilabs.net>
2020-12-07 12:08:10 +0100

    day7 2020

--- 2020/day7/src/main.rs
+++ 2020/day7/src/main.rs
@@ -1,35 +1,75 @@
-use std::collections::HashMap;
 use regex::Regex;
+use std::collections::HashMap;
 
 fn main() {
     let entries = helper::read_file_strings().unwrap();
 
-    part1(&entries);
-    // part2(&entries);
-}
+    let mut rules: HashMap<String, Vec<(usize, String)>> = HashMap::new();
+    let contains_bags_re = Regex::new(r"^(\d+) ([a-z ]+)( bags?.?)").unwrap();
 
-fn part1(entries: &Vec<String>) {
-  let rules: HashMap<String, Vec<String>> = HashMap::new();
-  let contains_bags_re = Regex::new(r"^(\d+)\s([a-z\s]+) bags?\.$").unwrap();
+    for l in entries {
+        let rule: Vec<&str> = l.split(" bags contain ").collect();
 
-  for l in entries {
-    let rule: Vec<&str> = l.split(" bags contain ").collect();
+        if rule[1] == "no other bags." {
+            continue;
+        }
 
-    if rule[1] == "no other bags." {
-      return;
+        let contains_bags: Vec<(usize, String)> = rule[1]
+            .split(", ")
+            .map(|c| {
+                let cap = contains_bags_re.captures(c).unwrap();
+                (cap[1].parse::<usize>().unwrap(), cap[2].to_string())
+            })
+            .collect();
+
+        rules.insert(rule[0].to_string(), contains_bags);
     }
 
-    println!("Part 1: {:?}.", rule);
-    let contains_bags: Vec<(usize, String)> = rule[1].split(", ").map(|c| {
-      let cap = contains_bags_re.captures(c).unwrap();
-      (cap[0].parse::<usize>().unwrap(), cap[1].to_string())
-    }).collect();
-    println!("Part 1: {:?}.", contains_bags);
-  }
-
-    println!("Part 1: {:?}.", rules);
+    part1(&rules);
+    part2(&rules);
 }
 
-// fn part2(entries: &Vec<String>) {
-//     println!("Part 2: {}.", entries);
-// }
+fn part1(rules: &HashMap<String, Vec<(usize, String)>>) {
+    let mut possible_bags_containing_shiny_gold = 0;
+    for (bag, _) in rules {
+        if bag != "shiny gold" && can_contain_shiny_gold_bag(bag, rules) {
+            possible_bags_containing_shiny_gold += 1;
+        }
+    }
+
+    println!("Part 1: {:?}.", possible_bags_containing_shiny_gold);
+}
+
+fn part2(rules: &HashMap<String, Vec<(usize, String)>>) {
+    println!("Part 2: {:?}.", get_containing_bags("shiny gold", rules));
+}
+
+fn can_contain_shiny_gold_bag(bag: &str, rules: &HashMap<String, Vec<(usize, String)>>) -> bool {
+    if bag == "shiny gold" {
+        return true;
+    }
+
+    match rules.get(bag) {
+        Some(containing_bags) => {
+            return containing_bags
+                .iter()
+                .any(|(_, b)| can_contain_shiny_gold_bag(b, rules))
+        }
+        None => return false,
+    }
+}
+
+fn get_containing_bags(bag: &str, rules: &HashMap<String, Vec<(usize, String)>>) -> usize {
+    match rules.get(bag) {
+        Some(containing_bags) => {
+            return containing_bags
+                .iter()
+                .map(|(count, b)| match get_containing_bags(b, rules) {
+                    0 => return count * 1,
+                    containing_bags_count => count * containing_bags_count + count,
+                })
+                .fold(0, |acc, x| acc + x)
+        }
+        None => return 0,
+    }
+}
