02a60b65f8232028d92e0b75e11f40ffcf1c06f2
Robert Katzki <robert@katzki.de>
2020-12-14 22:39:52 +0100

    day 13 progress

--- 2020/day13/src/main.rs
+++ 2020/day13/src/main.rs
@@ -2,10 +2,10 @@ use std::time::Instant;
 
 fn main() {
     let mut now = Instant::now();
-    let time = 939;
-    let bus_lines = "7,13,x,x,59,x,31,19";
-    // let time = 1015292;
-    // let bus_lines = "19,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,743,x,x,x,x,x,x,x,x,x,x,x,x,13,17,x,x,x,x,x,x,x,x,x,x,x,x,x,x,29,x,643,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,23";
+    // let time = 939;
+    // let bus_lines = "7,13,x,x,59,x,31,19";
+    let time = 1015292;
+    let bus_lines = "19,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,743,x,x,x,x,x,x,x,x,x,x,x,x,13,17,x,x,x,x,x,x,x,x,x,x,x,x,x,x,29,x,643,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,23";
 
     part1(time, bus_lines);
     println!("Time: {}µs", now.elapsed().as_micros());
@@ -42,26 +42,32 @@ fn part1(time: u32, raw_bus_lines: &str) {
 }
 
 fn part2(raw_bus_lines: &str) {
-    let rules: Vec<u32> = raw_bus_lines
+    let rules: Vec<(usize, u32)> = raw_bus_lines
         .split(",")
-        .map(|c| match c {
-            "x" => 1,
-            _ => c.parse::<u32>().unwrap(),
+        .enumerate()
+        .map(|(i, c)| match c {
+            "x" => (i, 0),
+            _ => (i, c.parse::<u32>().unwrap()),
         })
+        .filter(|(_, c)| c > &0u32)
         .collect();
 
-    let max_bus_id = rules.iter().max().expect("No max found!");
+    let max_bus_id = rules.iter().map(|(_, c)| c).max().expect("No max found!");
+
     let start = max_bus_id
         - rules
             .iter()
-            .position(|c| c == max_bus_id)
+            .position(|(_, c)| c == max_bus_id)
             .expect("Position of bus id not found!") as u32;
+
+    println!("{:?}", rules);
 
     let time = (start..)
         .into_iter()
         .step_by(*max_bus_id as usize)
-        .inspect(|i| println!("{:?}", i))
-        .find(|i| rules.iter().all(|r| i % r == 0));
+        // .find(|i| (i + 4) % 59 == 0 && (i + 6) % 31 == 0)
+        .find(|i| rules.iter().all(|(k, c)| (i + *k as u32) % c == 0))
+        .expect("Could not find a time.  :(");
 
     println!("Part 2: {:?}.", time);
 }
