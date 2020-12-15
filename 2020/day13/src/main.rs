7d265e05dd6ae63ecc90226960c19c98d74fc153
Robert Katzki <katzki@ubilabs.net>
2020-12-15 16:42:52 +0100

    improve day13

--- 2020/day13/src/main.rs
+++ 2020/day13/src/main.rs
@@ -1,72 +1,71 @@
 use std::time::Instant;
 
+static TIME: u32 = 939;
+static BUS_LINES: &str = "7,13,x,x,59,x,31,19";
+
+// static TIME: u32 = 1015292;
+// static BUS_LINES: &str = "19,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,743,x,x,x,x,x,x,x,x,x,x,x,x,13,17,x,x,x,x,x,x,x,x,x,x,x,x,x,x,29,x,643,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,23";
+
 fn main() {
     let mut now = Instant::now();
-    // let time = 939;
-    // let bus_lines = "7,13,x,x,59,x,31,19";
-    let time = 1015292;
-    let bus_lines = "19,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,743,x,x,x,x,x,x,x,x,x,x,x,x,13,17,x,x,x,x,x,x,x,x,x,x,x,x,x,x,29,x,643,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,23";
 
-    part1(time, bus_lines);
+    part1();
     println!("Time: {}µs", now.elapsed().as_micros());
     now = Instant::now();
 
-    part2(bus_lines);
+    part2();
     println!("Time: {}µs", now.elapsed().as_micros());
 }
 
-fn part1(time: u32, raw_bus_lines: &str) {
-    let bus_lines: Vec<u32> = raw_bus_lines
+fn part1() {
+    let (bus_id, time_to_wait) = BUS_LINES
         .split(",")
         .filter(|c| c != &"x")
         .map(|c| c.parse::<u32>().unwrap())
-        .collect();
-    let next_bus_at = bus_lines
-        .iter()
-        .map(|i| {
-            let mut departure_time = 0;
-            while departure_time < time {
-                departure_time += i
-            }
-            departure_time
-        })
-        .min()
+        .map(|i| (i, i - (TIME % i)))
+        .min_by(|(_, x), (_, y)| x.cmp(y))
         .expect("No departure time found!");
-    let bus_id = bus_lines
-        .iter()
-        .find(|i| next_bus_at % **i == 0)
-        .expect("No bus id found!");
-    let time_to_wait = next_bus_at - time;
 
     println!("Part 1: {:?}.", bus_id * time_to_wait);
 }
 
-fn part2(raw_bus_lines: &str) {
-    let rules: Vec<(usize, u32)> = raw_bus_lines
+fn part2() {
+    let mut rules: Vec<(usize, u64)> = BUS_LINES
         .split(",")
         .enumerate()
-        .map(|(i, c)| match c {
-            "x" => (i, 0),
-            _ => (i, c.parse::<u32>().unwrap()),
+        .map(|(index, n)| match n {
+            "x" => (index, 0),
+            _ => (index, n.parse::<u64>().unwrap()),
         })
-        .filter(|(_, c)| c > &0u32)
+        .filter(|(_, n)| n > &0u64)
         .collect();
 
-    let max_bus_id = rules.iter().map(|(_, c)| c).max().expect("No max found!");
+    rules.sort_by(|(_, a), (_, b)| a.cmp(b));
+    rules.reverse();
 
-    let start = max_bus_id
-        - rules
-            .iter()
-            .position(|(_, c)| c == max_bus_id)
-            .expect("Position of bus id not found!") as u32;
+    let (max_index, max_id) = rules[0];
+    // let (second_max_index, second_max_id) = rules[1];
 
-    println!("{:?}", rules);
+    let start = max_id - max_index as u64;
+
+    // let lcm: Vec<u64> = (max_id as u64..100000)
+    //     .into_iter()
+    //     .step_by(max_id as usize)
+    //     .filter(|i| second_max_id - (i % second_max_id) == (second_max_index - max_index) as u64)
+    //     .collect();
+    // // .expect("lcm not found!");
+
+    // println!("START {:?}", start);
+    // println!("LCM {:?}", lcm);
 
     let time = (start..)
         .into_iter()
-        .step_by(*max_bus_id as usize)
-        // .find(|i| (i + 4) % 59 == 0 && (i + 6) % 31 == 0)
-        .find(|i| rules.iter().all(|(k, c)| (i + *k as u32) % c == 0))
+        .step_by(max_id as usize)
+        .find(|i| {
+            rules
+                .iter()
+                .all(|(index, id)| (i + *index as u64) % id == 0)
+        })
         .expect("Could not find a time.  :(");
 
     println!("Part 2: {:?}.", time);
