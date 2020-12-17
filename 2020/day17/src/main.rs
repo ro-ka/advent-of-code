bc064d08aa97b00615a69f176218241a8f369ad6
Robert Katzki <katzki@ubilabs.net>
2020-12-17 12:48:29 +0100

    day17 part2 2020

--- 2020/day17/src/main.rs
+++ 2020/day17/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     println!("Time: {}µs", now.elapsed().as_micros());
     now = Instant::now();
 
-    // part2(&entries);
+    part2(&entries);
     println!("Time: {}µs", now.elapsed().as_micros());
 }
 
@@ -24,17 +24,31 @@ fn part1(entries: &Vec<String>) {
     });
 
     for _ in 0..6 {
-        state = cycle(state.clone());
+        state = cycle_3_dimensions(state.clone());
     }
 
     println!("Part 1: {:?}.", state.len());
 }
 
-// fn part2(entries: &Vec<String>) {
-//     println!("Part 2: {:?}.", entries);
-// }
+fn part2(entries: &Vec<String>) {
+    let mut state: Vec<(i32, i32, i32, i32)> = vec![];
 
-fn cycle(state: Vec<(i32, i32, i32)>) -> Vec<(i32, i32, i32)> {
+    entries.iter().enumerate().for_each(|(x, row)| {
+        row.chars().enumerate().for_each(|(y, c)| {
+            if c == '#' {
+                state.push((x as i32, y as i32, 0i32, 0i32));
+            }
+        });
+    });
+
+    for _ in 0..6 {
+        state = cycle_4_dimensions(state.clone());
+    }
+
+    println!("Part 2: {:?}.", state.len());
+}
+
+fn cycle_3_dimensions(state: Vec<(i32, i32, i32)>) -> Vec<(i32, i32, i32)> {
     let mut new_state: Vec<(i32, i32, i32)> = vec![];
 
     let x_values = state.iter().map(|(x, _, _)| x);
@@ -59,6 +73,45 @@ fn cycle(state: Vec<(i32, i32, i32)>) -> Vec<(i32, i32, i32)> {
 
                 if active_neighbors == 3 || (active_neighbors == 2 && currently_active) {
                     new_state.push(key);
+                }
+            }
+        }
+    }
+
+    new_state
+}
+
+fn cycle_4_dimensions(state: Vec<(i32, i32, i32, i32)>) -> Vec<(i32, i32, i32, i32)> {
+    let mut new_state: Vec<(i32, i32, i32, i32)> = vec![];
+
+    let x_values = state.iter().map(|(x, _, _, _)| x);
+    let y_values = state.iter().map(|(_, y, _, _)| y);
+    let z_values = state.iter().map(|(_, _, z, _)| z);
+    let w_values = state.iter().map(|(_, _, _, w)| w);
+
+    for x in (x_values.clone().min().unwrap() - 1)..=(x_values.clone().max().unwrap() + 1) {
+        for y in (y_values.clone().min().unwrap() - 1)..=(y_values.clone().max().unwrap() + 1) {
+            for z in (z_values.clone().min().unwrap() - 1)..=(z_values.clone().max().unwrap() + 1) {
+                for w in
+                    (w_values.clone().min().unwrap() - 1)..=(w_values.clone().max().unwrap() + 1)
+                {
+                    let key = (x, y, z, w);
+                    let currently_active = state.iter().find(|k| **k == key).is_some();
+                    let active_neighbors = state
+                        .iter()
+                        .map(|k| *k)
+                        .filter(|k| {
+                            *k != key
+                                && (k.0 - key.0).abs() <= 1
+                                && (k.1 - key.1).abs() <= 1
+                                && (k.2 - key.2).abs() <= 1
+                                && (k.3 - key.3).abs() <= 1
+                        })
+                        .count();
+
+                    if active_neighbors == 3 || (active_neighbors == 2 && currently_active) {
+                        new_state.push(key);
+                    }
                 }
             }
         }
