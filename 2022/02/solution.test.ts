5423ca9b3f7827d64dcc53b9c267d2ceb7f4e898
Robert Katzki <robert@katzki.de>
2022-12-04 20:39:48 +0100

    day2 part1 2022

--- 2022/02/solution.test.ts
+++ 2022/02/solution.test.ts
@@ -6,7 +6,7 @@ import { basename } from 'path';
 const exampleInput = readFileSync(`./${basename(__dirname)}/example.txt`).toString();
 
 test('part1 is correct', () => {
-  expect(part1(exampleInput)).toBe(undefined);
+  expect(part1(exampleInput)).toBe(15);
 })
 
 test('part2 is correct', () => {
--- 2022/02/solution.ts
+++ 2022/02/solution.ts
@@ -1,4 +1,20 @@
+const points = {
+  'A X': 1 + 3,
+  'A Y': 2 + 6,
+  'A Z': 3 + 0,
+  'B X': 1 + 0,
+  'B Y': 2 + 3,
+  'B Z': 3 + 6,
+  'C X': 1 + 6,
+  'C Y': 2 + 0,
+  'C Z': 3 + 3,
+}
+
 export function part1(input: string) {
+  return input
+    .split('\n')
+    .map(row => points[row])
+    .reduce((sum, point) => sum + point, 0);
 }
 
 export function part2(input: string) {
